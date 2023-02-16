d97f92cd5cc0ccd44692bea5a9c27ea19c2eda69
Your Name <you@example.com>
2023-02-17 00:04:28 +0530

    update

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -3,6 +3,7 @@ import { Todo } from './model';
 import { AiFillEdit, AiFillDelete, } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
 import "./styles.css";
+import { TodoList } from './TodoList';
 
 type Props = {
     todo: Todo;
@@ -14,25 +15,44 @@ type Props = {
 
 const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
 
-    return (
-        <>
-            <form className='todos__single'>
-                <span className='todos__single--text'>{todo.todo}</span>
+    const handleDone = (id: number) => {
+        
+            setTodos(
+                todos.map((todo) =>
+                   todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+            )
+        );
+};
 
-                <div>
-                    <span className='icon'>
-                        <AiFillEdit />
-                    </span>
-                    <span className='icon'>
-                        <AiFillDelete />
-                    </span>
-                    <span className='icon'>
-                        <MdDone />
-                    </span>
-                </div>
-            </form>
-        </>
-    )
+const handleDelete = (id: number) =>{
+    setTodos(todos.filter((todo) => todo.id !== id));
+};
+
+return (
+    <>
+        <form className='todos__single'>
+            {todo.isDone ? (
+                <s className='todos__single--text'>{todo.todo}</s>
+
+            ) : (
+
+                <span className='todos__single--text'>{todo.todo}</span>
+            )}
+
+            <div>
+                <span className='icon'>
+                    <AiFillEdit />
+                </span>
+                <span className='icon' onClick={() => handleDelete(todo.id)}>
+                    <AiFillDelete />
+                </span>
+                <span className='icon' onClick={() => handleDone(todo.id)}>
+                    <MdDone />
+                </span>
+            </div>
+        </form>
+    </>
+)
 }
 
 export { SingleTodo }; 
